bc5f688298a1ad5b5c157153288461c79be5f7c7
AlkhazovAvdalim <avdalim@outlook.com>
2023-12-10 13:30:18 +0100

    added comments explaining some lines

--- web/react-todo/src/components/Todo.jsx
+++ web/react-todo/src/components/Todo.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-
+// Function is passed into the component
 function Todo({ text, id, onClick }) {
   const [todo, setTodo] = useState("");
+  // Here is the function
   function handleClick() {}
   return (
     <div data-testid={id} key={id} className="todoEntry">
@@ -9,6 +10,7 @@ function Todo({ text, id, onClick }) {
       <button
         data-testid={"eval-item-done-" + id}
         className="todoEntryButton"
+        // Function defined from parent and passed via Props
         onClick={onClick}
       >
         ✅
--- web/react-todo/src/components/TodoField.jsx
+++ web/react-todo/src/components/TodoField.jsx
@@ -3,49 +3,51 @@ import Todo from "./Todo";
 
 function TodoField() {
   let todos = [];
-  // click of the button
+  // Click of the button from the input field
   const [todo, setTodo] = useState("");
   // Text from input
   const [text, setText] = useState("");
-  // List of todos
+  // Keeps track of todos, is a list with the todo entries, which gets updated with added todos
   const [items, setItems] = useState([]);
 
   function deleteTodo(id) {
+    // Handles the click, when the done button click is clicked, filters all the todos
+    // except the one clicked
     setItems(items.filter((item) => item !== id));
   }
 
   function handleClick() {
     let inputText = text;
     console.log(text);
+    // Takes the items from the previous list and adds the inputted text to the list
     setItems((prevList) => [...prevList, inputText]);
-    // <Todo text={inputText} onClick={() => deleteTodo({ inputText })} />,
-    // setItems((prevList)=>[...prevList,{{inputText}})
     setText("");
   }
 
   return (
     <>
+      {/* The List gets taken, iterated on and passed on to the constructor of the object
+    The deleteTodo function is also mapped onto the todo id */}
       {items.map((todo, index) => {
         return (
           <Todo
             key={todo}
             id={todo}
             text={todo}
+            // () => is necessary here as otherwise the todo is directly deleted.
+            // () => leads to the function being activated on click and not directly on lead
             onClick={() => deleteTodo(todo)}
           />
         );
       })}
       <div className="todoField">
-        {/* <button className="todoButton" onClick={console.log("CLicked")}>
-        {" "}
-        &#10133;
-      </button> */}
         <input
           type="text"
           className="inputField"
           placeholder="Enter new Todo"
           data-testid="eval-input-field"
           value={text}
+          // Changes the Text state, will be then added to list and deleted from input field on click
           onChange={(e) => setText(e.target.value)}
         ></input>
         <button data-testid="eval-add-todo-button" onClick={handleClick}>
